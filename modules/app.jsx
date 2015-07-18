053ae8592311b48f01ee194ed2c063eb592e0ded
Arnaud Pichon <arnaud@onesime.fr>
2015-07-18 15:15:59 +0200

    #notMyJavascript

---
+++ modules/app.jsx
@@ -0,0 +1,5 @@
+import "babel/polyfill";
+import React from "react";
+import d3 from "d3";
+
+import style from "./_stylesheet.scss";
--- modules/constants/particle-constants.js
+++
@@ -1,7 +0,0 @@
-import keyMirror from 'keymirror';
-
-export default keyMirror({
-  PARTICLE_CREATE: null,
-  PARTICLE_SELECT: null,
-  PARTICLE_DESTROY: null
-});
