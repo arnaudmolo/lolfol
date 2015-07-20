83e1bd037f5e1adfd97af331ed6b2faa1e73bfc8
Arnaud Pichon <arnaud@onesime.fr>
2015-07-20 18:34:18 +0200

    ca bouge

--- modules/components/moi/index.jsx
+++ modules/components/moi/index.jsx
@@ -1,4 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import {Spring} from 'react-motion';
+import {range} from 'd3';
+import autobind from 'autobind-decorator';
+
+var once = 0;
 
 const style = {
   width: '100px',
@@ -8,19 +13,53 @@ const style = {
   top: 0
 };
 
-export default class Moi extends Component {
+const onMouseMove = Symbol();
 
-  static propTypes = {
-    changeIndex: PropTypes.func.isRequired,
-    face: PropTypes.string.isRequired,
-    counter: PropTypes.number.isRequired
+class Moi extends Component {
+
+  constructor(props, context) {
+    super(props, context)
+    this.state = {mouse: {top: 0, left: 0}};
+    this.getValues = this.getValues.bind(this);
+    // this.setState  = this.setState.bind(this);
+    this[onMouseMove] = this[onMouseMove].bind(this);
+  }
+
+  getValues(currentPositions) {
+    return {val: this.state.mouse};
+  }
+
+  [onMouseMove]({pageX, pageY}) {
+    this.setState({mouse: {top: pageX, left: pageY}});
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
   }
 
   render() {
     const {circular, counter} = this.props;
     return (
-      <img style={style} src={`./img/face-${counter.get('counter')}.jpg`} width={"100%"} onClick={circular} />
+      <div className="container"
+        style={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "cyan"
+        }}
+        onMouseMove={this[onMouseMove]}
+        onClick={circular}
+      >
+        <Spring
+          endValue={this.getValues}
+        >
+          {({val}) => {
+            return (<img style={{...style, ...val}} src={`./img/face-${counter.get('counter')}.jpg`} width={"100%"} />)
+          }}
+        </Spring>
+      </div>
     );
   }
 
 }
+
+export default Moi
