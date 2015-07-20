322cffe22fdcc116cd792b306253e11a5f08ecfe
Arnaud Pichon <arnaud@onesime.fr>
2015-07-20 16:50:48 +0200

    gogoanimation

--- modules/components/moi/index.jsx
+++ modules/components/moi/index.jsx
@@ -1,5 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 
+const style = {
+  width: '100px',
+  height: '100px',
+  position: 'absolute',
+  left: 0,
+  top: 0
+};
+
 export default class Moi extends Component {
 
   static propTypes = {
@@ -10,13 +18,6 @@ export default class Moi extends Component {
 
   render() {
     const {circular, counter} = this.props;
-
-    const style = {
-      // backgroundImage: `url(./img/face-${counter.get('counter')}.jpg)`,
-      width: '100vw',
-      height: '100vh',
-    }
-
     return (
       <img style={style} src={`./img/face-${counter.get('counter')}.jpg`} width={"100%"} onClick={circular} />
     );
