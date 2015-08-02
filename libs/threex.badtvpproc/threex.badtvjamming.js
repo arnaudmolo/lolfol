864bd9f2af80f353a7215ee2d12de5de3ccaba2c
Arnaud Pichon <arnaud@onesime.fr>
2015-08-02 22:53:45 +0200

    effects

--- libs/threex.badtvpproc/threex.badtvjamming.js
+++ libs/threex.badtvpproc/threex.badtvjamming.js
@@ -16,70 +16,60 @@
 
 import BadTVSound from './threex.badtvsound'
 
-export default function(badTVPasses, context, destination){
-	destination	= destination	|| context.destination
-	
-	var badTVSound	= new BadTVSound(context, destination)
+export default class BadTVJamming {
 
+  /**
+   * true if a jamming is in progress, false otherwise
+   * @type {Boolean}
+   */
+  inProgress = false
+  presets = {
+    'lightNoScroll' : ['light' , 'resetInterlace' ,  2, 0.1],
+    'strongScrolly' : ['strong', 'resetInterlace' , 10, 0.10]
+  }
 
-	/**
-	 * true if a jamming is in progress, false otherwise
-	 * @type {Boolean}
-	 */
-	this.inProgress	= false
-	this.trigger	= function(passesFirstLabel, passesLastLabel, nShakeSteps, tweenDelay){
-		// default values for arguments
-		console.assert( typeof(passesFirstLabel) === 'string' )
-		console.assert( typeof(passesLastLabel) === 'string' )
-		console.assert( typeof(nShakeSteps) === 'number' )
-		console.assert( typeof(tweenDelay) === 'number' )
-		// prevent another trigger when a jamming is in progress
-		if( this.inProgress === true )	return
-		this.inProgress	= true
-		// start the sound
-		badTVSound.play()
-		
-		// set initial params
-		badTVPasses.params.preset(passesFirstLabel)
-		badTVPasses.onParamsChange()
-		
-		// handle badTVPasses.params animation
-		badTVPasses.tweenDelay	= tweenDelay
+  constructor(badTVPasses, context, destination) {
+    destination = destination || context.destination
+    this.badTVSound = new BadTVSound(context, destination)
+    this.badTVPasses = badTVPasses
+  }
 
-		badTVPasses.addEventListener('tweenCompleted', function callback(){
-			// if it is the last shake, return now
-			if( nShakeSteps === 0 )	return
-			// count this shake
-			nShakeSteps	-= 1;
-			if( nShakeSteps > 0 ){
-				// start the next shake
-				badTVPasses.params.preset(passesFirstLabel)			
-			}else{
-				// end all shake
-				badTVPasses.params.preset(passesLastLabel)
-				badTVPasses.removeEventListener('tweenCompleted', callback)
-				this.inProgress	= false
-			}
-			badTVPasses.onParamsChange()		
-		}.bind(this))
-	}
+  trigger(passesFirstLabel, passesLastLabel, nShakeSteps, tweenDelay) {
+    console.assert( typeof(passesFirstLabel) === 'string' )
+    console.assert( typeof(passesLastLabel) === 'string' )
+    console.assert( typeof(nShakeSteps) === 'number' )
+    console.assert( typeof(tweenDelay) === 'number' )
 
+    if (this.inProgress) {
+      return
+    }
 
-	//////////////////////////////////////////////////////////////////////////////////
-	//		presets								//
-	//////////////////////////////////////////////////////////////////////////////////
+    this.inProgress = true
+    this.badTVSound.play()
+    this.badTVPasses.params.preset(passesFirstLabel)
+    this.badTVPasses.onParamsChange()
+    this.badTVPasses.tweenDelay = tween
 
-	/**
-	 * Presets you can use
-	 * @type {Object}
-	 */
-	this.presets	= {
-		'lightNoScroll'	: ['light' , 'resetInterlace'	,  2, 0.1],
-		'strongScrolly'	: ['strong', 'resetInterlace'	, 10, 0.10]
-	}
-	this.preset	= function(label){
-		console.assert(label in this.presets === true)
-		var presetArgs	= this.presets[label]
-		this.trigger.apply(this, presetArgs)
-	}
+    this.badTVPasses.addEventListener('tweenCompleted', () => {
+      if( nShakeSteps === 0 ) return
+      // count this shake
+      nShakeSteps -= 1;
+      if( nShakeSteps > 0 ){
+        // start the next shake
+        badTVPasses.params.preset(passesFirstLabel)     
+      }else{
+        // end all shake
+        badTVPasses.params.preset(passesLastLabel)
+        badTVPasses.removeEventListener('tweenCompleted', callback)
+        this.inProgress = false
+      }
+      badTVPasses.onParamsChange()    
+    })
+  }
+
+  preset(label) {
+    console.assert(label in this.presets === true)
+    var presetArgs  = this.presets[label]
+    this.trigger.apply(this, presetArgs)
+  }
 }
--- modules/threetest.js
+++ modules/threetest.js
@@ -1,18 +1,18 @@
-import Three, {
+import {
   WebGLRenderer,
   Scene,
-  PerspectiveCamera,
-  CubeGeometry,
+  OrthographicCamera,
+  PlaneGeometry,
   MeshNormalMaterial,
   Mesh,
+  Texture,
+  NearestFilter,
+  ImageUtils,
 } from 'three'
-
-window.THREE = Three
-
 import EffectComposer from './../libs/utils/EffectComposer'
 import RenderPass from './../libs/utils/RenderPass'
-
 import BadTVPasses from './../libs/threex.badtvpproc/threex.badtvpasses'
+import BadTVJamming from './../libs/threex.badtvpproc/threex.badtvjamming'
 import addBadTVPasses2DatGui from './../libs/threex.badtvpproc/threex.badtvdatgui'
 
 const renderer = new WebGLRenderer()
@@ -23,19 +23,36 @@ document.body.appendChild(renderer.domElement)
 
 const onRenderFcts = []
 const scene = new Scene()
-const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.01, 1000)
+const camera = new OrthographicCamera(innerWidth / - 2, innerWidth / 2, innerHeight / 2, innerHeight / - 2, -10000, 10000 )
 
 camera.position.z = 3
 
-const geometry = new CubeGeometry(1, 1, 1)
-const material = new MeshNormalMaterial()
+var video    = document.createElement( 'video' );
+video.loop   = true;
+video.volume = 0;
+video.src    = "videos/fits.mp4";
+video.play();
+// create the texture
+var texture = new Texture( video );
+texture.minFilter   = NearestFilter;
+texture.magFilter   = NearestFilter;
+onRenderFcts.push(function(delta, now){
+    if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+        if ( texture ) texture.needsUpdate = true;
+    }       
+})
+
+const geometry = new PlaneGeometry(innerWidth, innerHeight)
+const material = new MeshNormalMaterial({map: texture})
 const mesh     = new Mesh(geometry, material)
 
 scene.add(mesh)
 
 const badTVPasses = new BadTVPasses()
 
-onRenderFcts.push(badTVPasses.update)
+onRenderFcts.push(function(delta, now) {
+  badTVPasses.update(delta, now)
+})
 
 addBadTVPasses2DatGui(badTVPasses)
 
@@ -46,34 +63,32 @@ composer.addPass(renderPass)
 badTVPasses.addPassesTo(composer)
 composer.passes[composer.passes.length - 1].renderToScreen = true
 
-const mouse = {x: 0, y: 0}
+const context = new AudioContext()
+const badTVJamming = new BadTVJamming(badTVPasses, context)
 
-document.addEventListener('mousemove', function(event){
-  console.log("log");
-  mouse.x = (event.clientX / window.innerWidth ) - 0.5
-  mouse.y = (event.clientY / window.innerHeight) - 0.5
-}, false)
-
-onRenderFcts.push(function(delta, now){
-  camera.position.x += (mouse.x*5 - camera.position.x) * (delta*3)
-  camera.position.y += (mouse.y*5 - camera.position.y) * (delta*3)
-  camera.lookAt(scene.position)
+document.querySelector('canvas').addEventListener('click', function() {
+  badTVJamming.preset('lightNoScroll')
 })
 
+window.switchValue = function(value) {
+  badTVJaming.preset(value)
+}
+
 onRenderFcts.push(function(delta) {
-  console.log(composer.render);
   composer.render(delta)
 })
 
 let lastTimeMsec = null
 
-requestAnimationFrame(function animate(nowMsec) {
-  requestAnimationFrame(animate)
-  lastTimeMsec    = lastTimeMsec || nowMsec-1000/60
-  var deltaMsec   = Math.min(200, nowMsec - lastTimeMsec)
-  lastTimeMsec    = nowMsec
-  // call each update function
-  onRenderFcts.forEach(function(onRenderFct){
-    onRenderFct(deltaMsec/1000, nowMsec/1000)
-  })
+requestAnimationFrame(function animate(nowMsec){
+    // keep looping
+    requestAnimationFrame(animate);
+    // measure time
+    lastTimeMsec    = lastTimeMsec || nowMsec-1000/60
+    var deltaMsec   = Math.min(200, nowMsec - lastTimeMsec)
+    lastTimeMsec    = nowMsec
+    // call each update function
+    onRenderFcts.forEach(function(onRenderFct){
+        onRenderFct(deltaMsec/1000, nowMsec/1000)
+    })
 })
